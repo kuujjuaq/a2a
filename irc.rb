38c678b1b8e47bbe7022a59ce6327f05633c80ed
Kevin Doherty <kdoherty@Kevin-Dohertys-MacBook-Pro-17.local>
2012-09-14 02:38:11 -0400

    Added stub for em-irc stuff

--- irc.rb
+++ irc.rb
@@ -1,5 +1,26 @@
 require 'rubygems'
+require 'em-irc'
 require 'game'
+require 'lobby'
+require 'config'
 
 class A2A::IRC
+  def initialize()
+    @cfg = A2A::Config.new
+    @game = nil
+    
+    @lobby = A2A::Lobby.new(@cfg.chan)
+    @client = EventMachine::IRC::Client.new do |c|
+      host @cfg.host
+      port @cfg.port
+      
+      on (:connect) { nick(@cfg.nick) }
+
+      on (:nick) { join(@cfg.chan) } 
+      
+      on(:message) do |src, tgt, msg|
+        c.message(@cfg.chan, "got message #{msg}")
+      end
+    end
+  end
 end
